d7a6041e33eaaa4c713f531c6076b1af5540ed20
simeddk <65766852+simeddk@users.noreply.github.com>
2022-04-19 15:32:45 +0900

    0418_Triangle 2개 그려보기 - 2

--- D2D/D2D/Scene.cpp
+++ D2D/D2D/Scene.cpp
@@ -1,6 +1,7 @@
 #include "Device.h"
 
 ID3D11Buffer* vertexBuffer = nullptr;
+ID3D11Buffer* vertexBuffer2 = nullptr;
 ID3D11InputLayout* inputLayout = nullptr;
 
 struct Vertex
@@ -11,9 +12,8 @@ struct Vertex
 
 void InitScene()
 {
-	Vertex vertices[6];
-
 	//Tri - 1
+	Vertex vertices[3];
 	vertices[0].Position = D3DXVECTOR3(-0.5f, +0.0f, 0.0f);
 	vertices[1].Position = D3DXVECTOR3(+0.0f, +1.0f, 0.0f);
 	vertices[2].Position = D3DXVECTOR3(+0.5f, +0.0f, 0.0f);
@@ -23,13 +23,14 @@ void InitScene()
 	vertices[2].Color = D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f);
 
 	//Tri - 2
-	vertices[3].Position = D3DXVECTOR3(-0.5f, -1.0f, 0.0f);
-	vertices[4].Position = D3DXVECTOR3(+0.0f, +0.0f, 0.0f);
-	vertices[5].Position = D3DXVECTOR3(+0.5f, -1.0f, 0.0f);
+	Vertex vertices2[3];
+	vertices2[0].Position = D3DXVECTOR3(-0.5f, -1.0f, 0.0f);
+	vertices2[1].Position = D3DXVECTOR3(+0.0f, +0.0f, 0.0f);
+	vertices2[2].Position = D3DXVECTOR3(+0.5f, -1.0f, 0.0f);
 
-	vertices[3].Color = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
-	vertices[4].Color = D3DXCOLOR(0.0f, 1.0f, 1.0f, 1.0f);
-	vertices[5].Color = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
+	vertices2[0].Color = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
+	vertices2[1].Color = D3DXCOLOR(0.0f, 1.0f, 1.0f, 1.0f);
+	vertices2[2].Color = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
 
 
 	//Create VertexBuffer
@@ -37,13 +38,28 @@ void InitScene()
 		D3D11_BUFFER_DESC desc;
 		ZeroMemory(&desc, sizeof(D3D11_BUFFER_DESC));
 		desc.Usage = D3D11_USAGE_DEFAULT;
-		desc.ByteWidth = sizeof(Vertex) * 6;
+		desc.ByteWidth = sizeof(Vertex) * 3;
 		desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 
 		D3D11_SUBRESOURCE_DATA subResource = { 0 };
 		subResource.pSysMem = vertices;
 
 		HRESULT hr = Device->CreateBuffer(&desc, &subResource, &vertexBuffer);
+		assert(SUCCEEDED(hr));
+	}
+
+	//Create VertexBuffer2
+	{
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(D3D11_BUFFER_DESC));
+		desc.Usage = D3D11_USAGE_DEFAULT;
+		desc.ByteWidth = sizeof(Vertex) * 3;
+		desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+		D3D11_SUBRESOURCE_DATA subResource = { 0 };
+		subResource.pSysMem = vertices2;
+
+		HRESULT hr = Device->CreateBuffer(&desc, &subResource, &vertexBuffer2);
 		assert(SUCCEEDED(hr));
 	}
 
@@ -89,6 +105,7 @@ void DestroyScene()
 {
 	inputLayout->Release();
 	vertexBuffer->Release();
+	vertexBuffer2->Release();
 }
 
 void Update()
@@ -107,7 +124,10 @@ void Render()
 		DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		DeviceContext->IASetInputLayout(inputLayout);
 
-		DeviceContext->Draw(6, 0);
+		DeviceContext->Draw(3, 0);
+
+		DeviceContext->IASetVertexBuffers(0, 1, &vertexBuffer2, &stride, &offset);
+		DeviceContext->Draw(3, 0);
 	}
 	SwapChain->Present(0, 0);
 }
