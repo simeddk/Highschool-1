824c7d133d8c564f97667063c57b5a5cba50bdff
simeddk <65766852+simeddk@users.noreply.github.com>
2022-04-20 17:04:06 +0900

    0420_IndexBuffer

--- D2D/D2D/Scene.cpp
+++ D2D/D2D/Scene.cpp
@@ -2,6 +2,7 @@
 #include "Device.h"
 
 ID3D11Buffer* vertexBuffer = nullptr;
+ID3D11Buffer* indexBuffer = nullptr;
 ID3D11InputLayout* inputLayout = nullptr;
 ID3D11RasterizerState* wireFrameMode = nullptr;
 
@@ -11,22 +12,23 @@ struct Vertex
 	D3DXCOLOR Color;
 };
 Vertex vertices[6];
+UINT indices[12];
 
 void InitScene()
 {
 	vertices[0].Position = D3DXVECTOR3(-0.5f, -0.5f, 0.0f); //좌하
 	vertices[1].Position = D3DXVECTOR3(-0.5f, +0.5f, 0.0f); //좌상
 	vertices[2].Position = D3DXVECTOR3(+0.5f, -0.5f, 0.0f); //우하
-	vertices[3].Position = D3DXVECTOR3(+0.5f, -0.5f, 0.0f); //우하
-	vertices[4].Position = D3DXVECTOR3(-0.5f, +0.5f, 0.0f); //좌상
-	vertices[5].Position = D3DXVECTOR3(+0.5f, +0.5f, 0.0f); //우상
-
-	vertices[0].Color = D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f); //R
-	vertices[1].Color = D3DXCOLOR(0.0f, 1.0f, 0.0f, 1.0f); //G
-	vertices[2].Color = D3DXCOLOR(0.0f, 0.0f, 1.0f, 1.0f); //B
-	vertices[3].Color = D3DXCOLOR(0.0f, 1.0f, 1.0f, 1.0f); //C
-	vertices[4].Color = D3DXCOLOR(1.0f, 0.0f, 1.0f, 1.0f); //M
-	vertices[5].Color = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f); //Y
+	vertices[3].Position = D3DXVECTOR3(+0.5f, +0.5f, 0.0f); //우상
+	vertices[4].Position = D3DXVECTOR3(+0.75f, -0.75f, 0.0f); //추가1
+	vertices[5].Position = D3DXVECTOR3(+0.75f, +0.75f, 0.0f); //추가2
+	
+	vertices[0].Color = D3DXCOLOR(1, 0, 0, 1); //R
+	vertices[1].Color = D3DXCOLOR(0, 1, 0, 1); //G
+	vertices[2].Color = D3DXCOLOR(0, 0, 1, 1); //B
+	vertices[3].Color = D3DXCOLOR(0, 1, 1, 1); //C
+	vertices[4].Color = D3DXCOLOR(1, 0, 1, 1); //M
+	vertices[5].Color = D3DXCOLOR(1, 1, 0, 1); //Y
 
 	//Create VertexBuffer
 	{
@@ -41,6 +43,34 @@ void InitScene()
 		subResource.pSysMem = vertices;
 
 		HRESULT hr = Device->CreateBuffer(&desc, &subResource, &vertexBuffer);
+		assert(SUCCEEDED(hr));
+	}
+
+	indices[0] = 0;
+	indices[1] = 1;
+	indices[2] = 2;
+	indices[3] = 2;
+	indices[4] = 1;
+	indices[5] = 3;
+	indices[6] = 2;
+	indices[7] = 3;
+	indices[8] = 4;
+	indices[9] = 4;
+	indices[10] = 3;
+	indices[11] = 5;
+
+	//Create IndexBuffer
+	{
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(D3D11_BUFFER_DESC));
+		desc.ByteWidth = sizeof(UINT) * 12;
+		desc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		desc.Usage = D3D11_USAGE_IMMUTABLE;
+
+		D3D11_SUBRESOURCE_DATA subResource = { 0 };
+		subResource.pSysMem = indices;
+
+		HRESULT hr = Device->CreateBuffer(&desc, &subResource, &indexBuffer);
 		assert(SUCCEEDED(hr));
 	}
 
@@ -96,6 +126,7 @@ void DestroyScene()
 {
 	inputLayout->Release();
 	vertexBuffer->Release();
+	indexBuffer->Release();
 }
 
 bool bWireFrameMode = false;
@@ -113,23 +144,23 @@ void Update()
 	//[0]정점 이동하기
 	if (Key->Press('A'))
 	{
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < ARRAYSIZE(vertices); i++)
 			vertices[i].Position.x -= 1e-4f;
 	}
 	else if (Key->Press('D'))
 	{
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < ARRAYSIZE(vertices); i++)
 			vertices[i].Position.x += 1e-4f;
 	}
 
 	if (Key->Press('S'))
 	{
-		for (int i = 0 ; i < 6; i++)
+		for (int i = 0 ; i < ARRAYSIZE(vertices); i++)
 			vertices[i].Position.y -= 1e-4f;
 	}
 	else if (Key->Press('W'))
 	{
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < ARRAYSIZE(vertices); i++)
 			vertices[i].Position.y += 1e-4f;
 	}
 
@@ -144,7 +175,7 @@ void Update()
 	D3D11_MAPPED_SUBRESOURCE subResouce;
 	DeviceContext->Map(vertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &subResouce);
 	{
-		memcpy(subResouce.pData, vertices, sizeof(Vertex) * 6);
+		memcpy(subResouce.pData, vertices, sizeof(Vertex) * ARRAYSIZE(vertices));
 	}
 	DeviceContext->Unmap(vertexBuffer, 0);
 }
@@ -157,12 +188,13 @@ void Render()
 		UINT stride = sizeof(Vertex);
 		UINT offset = 0;
 		DeviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+		DeviceContext->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
 		DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		DeviceContext->IASetInputLayout(inputLayout);
 
 		DeviceContext->RSSetState(bWireFrameMode ? wireFrameMode : nullptr);
 
-		DeviceContext->Draw(6, 0);
+		DeviceContext->DrawIndexed(12, 0, 0);
 	}
 	SwapChain->Present(0, 0);
 }
